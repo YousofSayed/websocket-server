fc684273d09ae52818f07cc9b629a81dac945edd
yousef-sayed <88796141+YousofSayed@users.noreply.github.com>
2023-05-17 23:00:54 +0300

    Update wss.js

--- wss.js
+++ wss.js
@@ -1,6 +1,6 @@
 import { createSimpleSecureWebsocketServer } from "https://deno.land/x/simple-secure-websocket-server/mod.ts";
 
-const socketHandler = (socket: WebSocket) => {
+const socketHandler = (socket) => {
   socket.onerror = (e) => console.error("socket error", e);
   socket.onopen = () => console.log("new socket connection", socket);
   socket.onclose = () => console.log("bye, socket connection", socket);
