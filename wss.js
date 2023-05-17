c34dd05b003ebd2a5da962d3041ae5d6ae208e4c
yousef-sayed <88796141+YousofSayed@users.noreply.github.com>
2023-05-17 22:58:23 +0300

    Create wss.js

---
+++ wss.js
@@ -0,0 +1,16 @@
+import { createSimpleSecureWebsocketServer } from "https://deno.land/x/simple-secure-websocket-server/mod.ts";
+
+const socketHandler = (socket: WebSocket) => {
+  socket.onerror = (e) => console.error("socket error", e);
+  socket.onopen = () => console.log("new socket connection", socket);
+  socket.onclose = () => console.log("bye, socket connection", socket);
+};
+
+const server = createSimpleSecureWebsocketServer({
+  socketHandler,
+  port: 8888,
+  certFile: "./certs/certfile.pem", // certfile path
+  keyFile: "./certs/keyfile.pem", // keyfile path
+});
+
+server.listen();
