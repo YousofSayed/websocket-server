9c1ea5902c1966bd023cb6a065d2c9f6bc675a90
Yousef Sayed <88796141+YousofSayed@users.noreply.github.com>
2023-09-01 21:21:47 -0700

    Update wss.js

--- wss.js
+++ wss.js
@@ -1,11 +1,14 @@
 import {WebSocketServer} from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 const ws = new WebSocketServer(9090);
+const sockets =[];
 
 ws.on('connection',(socket)=>{
     console.log('new user connected');
+    sockets.push(socket);
     
     socket.on('message', (data)=>{
-        console.log(data);
-        ws.send(data);
+        sockets.forEach((soc)=>{
+            soc.send(data)
+        })
     })
 })
