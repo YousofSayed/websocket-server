3c116c51a40ccd42488ffee4bc5163663f0bd5ac
Yousef Sayed <88796141+YousofSayed@users.noreply.github.com>
2023-12-13 01:47:43 +0200

    Update wss.js

--- wss.js
+++ wss.js
@@ -2,7 +2,7 @@ import {Server} from "npm:socket.io";
 
 const io = new Server(3000 , {
     cors:{
-        origin:'*'
+        origin:['https://thunder-c2t.pages.dev','http://localhost:5173']
     }
 });
 
